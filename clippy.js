d3e868e8423eecf09ac599893da63fa68b7ad210
Christian Kohler <kohler.christian@gmail.com>
2015-09-06 19:13:48 +0200

    feat(clippy): initial script added

---
+++ clippy.js
@@ -0,0 +1,22 @@
+(function() {
+	"use strict";
+	
+	let mappings = {
+		"65": agent => agent.show(),
+		"49": agent => agent.speak("Where am I?"), //1
+		"50": agent => agent.speak("Uh nice, hello Zuehlke!"), //2
+		"51": agent => agent.speak("You could use custom elements"), //3
+	};
+	
+	clippy.load('Clippy', registerKeyDownListener);
+	
+	function registerKeyDownListener(agent) {
+		document.addEventListener('keydown', function (event) {
+			if(mappings[event.keyCode]) {
+				mappings[event.keyCode](agent);
+			} else {
+				!agent.hidden && agent.hide();
+			}
+    	});
+	}
+})();
